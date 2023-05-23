142eb389bb577054ac28c79334cf373541a43115
CyrineZekri <cyrinezekri4@gmail.com>
2023-05-23 16:02:41 +0100

    header file

--- sort.h
+++ sort.h
@@ -1,3 +1,5 @@
+#ifndef INV_TREE_H
+#define INV_TREE_H
 #include <stdio.h>
 /**
  * struct listint_s - Doubly linked list node
@@ -15,3 +17,4 @@ typedef struct listint_s
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
+#endif
