a9896c0a2156140bfad6adf124a9407e37db5714
CyrineZekri <cyrinezekri4@gmail.com>
2023-05-26 16:25:49 +0100

    sorting by selection

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,29 +1,34 @@
 #include "sort.h"
 /**
+ * swap- sorting  algorithm
+ * @xp: The array to be sorted
+ * @yp: Number of elements in @array
+ */
+void swap(int *xp, int *yp)
+{
+	int temp = *xp;
+	*xp = *yp;
+	*yp = temp;
+}
+/**
  * selection_sort - sorting algorithm
  * @array: The array to be sorted
  * @size: Number of elements in @array
  */
 void selection_sort(int *array, size_t size)
 {
-    size_t i, j, min;
-    int swap;
-    for (i = 0; i < size - 1; i++)
-    {
-        min = array[0];
-        for (j = i + 1; j < size - 1; j++)
-        {
-            if (array[i] < array[min])
-            {
-                min =j;
-            }
-        }
-        if (min != i)
-        {
-            swap = array[i];
-            array[i] = array[min];
-            array[min] = swap;
-            print_array(array, size);
-        }
-    }
+	size_t i, j;
+	size_t min_idx;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min_idx = i;
+		for (j = i + 1; j < size; j++)
+			if (array[j] < array[min_idx])
+				min_idx = j;
+
+		if (min_idx != i)
+			swap(&array[min_idx], &array[i]);
+		print_array(array, size);
+	}
 }
