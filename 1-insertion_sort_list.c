2e9d1458467912e081f4973e5af8c681ef2ccfb1
CyrineZekri <cyrinezekri4@gmail.com>
2023-05-26 14:17:26 +0100

    betty

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -8,52 +8,29 @@ void insertion_sort_list(listint_t **list)
 	listint_t *first, *last, *tmp;
 
 	if (!list || !*list)
-
 		return;
-
 	last = (*list);
-
 	first = (*list)->next;
-
 	while (first)
-
 	{
-
 		while (last && first->n < last->n)
-
 		{
-
 			if (last->prev)
-
 				last->prev->next = first;
-
 			else
-
 				*list = first;
-
 			if (first->next)
-
 				first->next->prev = last;
-
 			tmp = first->next;
-
 			first->next = last;
-
 			first->prev = last->prev;
-
 			last->next = tmp;
-
 			last->prev = first;
-
 			print_list(*list);
-
 			last = first->prev;
 		}
-
 		first = first->next;
-
 		if (first)
-
 			last = first->prev;
 	}
 }
