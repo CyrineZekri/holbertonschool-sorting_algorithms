9c9d654429fb16215523341dc5550041481f58d0
CyrineZekri <cyrinezekri4@gmail.com>
2023-05-26 15:19:27 +0100

    selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -10,23 +10,20 @@ void selection_sort(int *array, size_t size)
     int swap;
     for (i = 0; i < size - 1; i++)
     {
-            int i, j, min_idx;
- 
-    // One by one move boundary of
-    // unsorted subarray
-    for (i = 0; i < n - 1; i++) {
- 
-        // Find the minimum element in
-        // unsorted array
-        min_idx = i;
-        for (j = i + 1; j < n; j++) {
-            if (arr[j] < arr[min_idx])
-                min_idx = j;
+        min = array[0];
+        for (j = i + 1; j < size - 1; j++)
+        {
+            if (array[i] < array[min])
+            {
+                min =
+            }
         }
- 
-        // Swap the found minimum element
-        // with the first element
-        if (min_idx != i)
-            swap(arr[min_idx], arr[i]);
+        if (min != i)
+        {
+            swap = array[i];
+            array[i] = array[min];
+            array[min] = swap;
+            print_array(array, size);
+        }
     }
 }
